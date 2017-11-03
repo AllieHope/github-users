f489d985ad99f68d3d417d639712112b59700105
Allison Rodriguez <allisonhope10@gmail.com>
2017-11-03 10:15:43 +0100

    style list and include profile pictures

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
   users$: Observable<string[]>;
